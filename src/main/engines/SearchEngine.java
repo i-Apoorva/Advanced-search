ec8a9479ee0dc59f25d95964c263dd24fc7c27cf
lucas <lucas2213690@gmail.com>
2017-04-19 23:03:50 -0300

    Added Configuration window and UserAgent options

--- src/main/engines/SearchEngine.java
+++ src/main/engines/SearchEngine.java
@@ -6,9 +6,9 @@ import main.model.SearchResult;
 
 public abstract class SearchEngine{
 	 
-	protected static final String DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36";
-	protected static final Integer DEFAULT_TIMEOUT = 8000;
-	protected static final Integer DEFAULT_PAGE = 1;
+	public static String DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36";
+	public static final Integer DEFAULT_TIMEOUT = 8000;
+	public static final Integer DEFAULT_PAGE = 1;
 	protected String userAgent;
 	protected String query;
 	protected Integer page;
